385f99b9caa1738f70a58d382eb8f0c7ac1dc49a
adityabhartib <adityabharti.ab@gmail.com>
2022-11-20 01:36:58 +0530

    Added : calculate multiple ride

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceGeneratorTest {
+
     @Test
     public void givenDistance_and_time_should_return_total_fare(){
         CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
@@ -16,4 +17,12 @@ public class CabInvoiceGeneratorTest {
         double total_fare= cabInvoiceGenerator.calculateTotalFair(0.1,4);
         Assertions.assertEquals(total_fare,5);
     }
+    @Test
+    public void given_multiple_rides_should_return_total_fare(){
+        CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
+        Ride [] rides = new Ride[]{new Ride(4, 6), new Ride(5, 8)};
+        double total_fare= cabInvoiceGenerator.calculateAggregateFair(rides);
+        Assertions.assertEquals(total_fare,104);
+    }
+
 }
