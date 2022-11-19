06ecd21f1c8f98b5a0b1c7ff37c717add5e78aa8
adityabhartib <adityabharti.ab@gmail.com>
2022-11-20 00:17:38 +0530

    Added : Calculate Fair Added

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -4,4 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceGeneratorTest {
+    @Test
+    public void givenDistance_and_time_should_return_total_fare(){
+        CabInvoiceGenerator cabInvoiceGenerator=new CabInvoiceGenerator();
+        double total_fare= cabInvoiceGenerator.calculateTotalFair(5,5);
+        Assertions.assertEquals(total_fare,55);
+    }
+
 }
