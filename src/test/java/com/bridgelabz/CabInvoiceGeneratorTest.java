e7e39ba9333c9bfa64de5b9143002acdd612b4b1
adityabhartib <adityabharti.ab@gmail.com>
2022-11-21 13:47:08 +0530

    Added : uc5 premium ride added

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -40,14 +40,31 @@ public class CabInvoiceGeneratorTest {
         Invoice expectedInvoice = new Invoice(66.5, 4, 266.0);
         Assertions.assertEquals(expectedInvoice, actualInvoice);
     }
+
     @Test
     public void givenUserIDShouldReturnInvoice() {
-        Ride[] rides1 = {new Ride(10,5),new Ride(0.1,3),new Ride(5,9),new Ride(9,8)};
-        Ride[] rides2 = {new Ride(10,6),new Ride(0.1,3),new Ride(5,9),new Ride(9,8)};
-        RideRepository.customerList.add(new Customer(1,rides1));
-        RideRepository.customerList.add(new Customer(2,rides2));
+        Ride[] rides1 = {new Ride(10, 5), new Ride(0.1, 3), new Ride(5, 9), new Ride(9, 8)};
+        Ride[] rides2 = {new Ride(10, 6), new Ride(0.1, 3), new Ride(5, 9), new Ride(9, 8)};
+        RideRepository.customerList.add(new Customer(1, rides1));
+        RideRepository.customerList.add(new Customer(2, rides2));
         Invoice actualInvoice = RideRepository.generateInvoiceByUserId(2);
-        Invoice expectedInvoice = new Invoice(66.75,4,267.0);
-        Assertions.assertEquals(expectedInvoice,actualInvoice);
+        Invoice expectedInvoice = new Invoice(66.75, 4, 267.0);
+        Assertions.assertEquals(expectedInvoice, actualInvoice);
+    }
+
+    @Test
+    public void givenPrmiunmRideShouldReturnInvoice() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double totalFare = cabInvoiceGenerator.calculateFarePREM(0.9, 1);
+        Assertions.assertEquals(20.0, totalFare);
+        System.out.println(totalFare);
+    }
+
+    @Test
+    public void givenNormalRideShouldReturnInvoice() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double totalFare = cabInvoiceGenerator.calculateTotalFair(0.1, 2);
+        Assertions.assertEquals(5.0, totalFare);
+        System.out.println(totalFare);
     }
 }
